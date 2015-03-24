8a9357a57eaf080a7b4a4315e5c7c068e7b5db82
Julien Guanzon <julienguanzon@gmail.com>
2015-03-23 22:17:07 -0600

    Initial Commit

---
+++ EventCheckIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EventCheckIn
+//
+//  Created by Julien Guanzon on 3/23/15.
+//  Copyright (c) 2015 Julien Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EventCheckIn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EventCheckIn
+//
+//  Created by Julien Guanzon on 3/23/15.
+//  Copyright (c) 2015 Julien Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
